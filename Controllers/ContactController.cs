9b06c2316e84622e86bd969d9ec26d17e5dd7c0b
vihangasak <vihangafts@gmail.com>
2024-10-17 20:24:16 +0530

    Contact Mail Submission Added

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SakwithiWebApp.Models;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+
+namespace SakwithiWebApp.Controllers
+{
+
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly IConfiguration _configuration;
+        public ContactController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Submit(ContactModel model) 
+        {
+            if (ModelState.IsValid) 
+            {
+                try
+                {
+                    SendEmail(model);
+                    TempData["SubmissionSuccessful"] = true;
+                    return RedirectToAction("ThankYou", model);
+                }
+                catch (Exception ex) { ModelState.AddModelError("", "An error occurred while sending the email. Please try again later."); }
+            }
+            return View("Index", model);
+        }
+
+        public IActionResult ThankYou(ContactModel model) 
+        {
+            if (TempData["SubmissionSuccessful"] == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            var smtpServer = _configuration["SmtpSettings:Server"];
+            var smtpPort = int.Parse(_configuration["SmtpSettings:Port"]);
+            var smtpUsername = _configuration["SmtpSettings:Username"];
+            var smtpPassword = _configuration["SmtpSettings:Password"];
+            var toEmail = _configuration["SmtpSettings:ToEmail"];
+
+            using (var client = new SmtpClient(smtpServer, smtpPort))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                client.EnableSsl = true;
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(smtpUsername),
+                    Subject = "New Contact Form Submission",
+                    Body = $"Name: {model.FirstName}_{model.LastName}\nEmail: {model.Email}\nMessage: {model.Message}",
+                    IsBodyHtml = false,
+                };
+                mailMessage.To.Add(toEmail);
+
+                client.Send(mailMessage);
+            }
+        }
+    }
+}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SakwithiWebApp.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name ="First name")]
+        public String FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [Display(Name = "Last name")]
+        public String LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public String Message { get; set; }
+
+        [Required(ErrorMessage = "Answer is required")]
+        [Display(Name = "I am not a Robot")]
+        public Boolean NotRbot { get; set; }
+    }
+}
